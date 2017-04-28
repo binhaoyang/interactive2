c45950d664515c133aaccd86f45020da09e81243
binhaoyang <macbook@MacBook-Pro-32.local>
2017-04-27 18:13:24 -0700

    p3
    p3

--- p3/assets/css/js/script.js
+++ p3/assets/css/js/script.js
@@ -3,3 +3,22 @@ $('.red-circle').hover(function() {
   }, function() {
      $('.marquee').start();
   });
+
+
+$('.top23').hover(function() {
+     $('.group1').fadeOut();
+  }, function() {
+     $('.group1').fadeIn();
+  });
+
+$('.top23').hover(function() {
+     $('.group2').fadeOut();
+  }, function() {
+     $('.group2').fadeIn();
+  });
+
+$('.top23').hover(function() {
+     $('.group3').fadeOut();
+  }, function() {
+     $('.group3').fadeIn();
+  });
